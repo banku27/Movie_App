fc9ec28c180e1fa7e1ed7c10ca8765c99b922bc7
banku27 <pankajparihar123321@gmail.com>
2022-05-15 11:24:01 +0530

    search widget

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,12 +6,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class MainScreen extends ConsumerWidget {
   double? _deviceHeight;
   double? _deviceWidth;
+
+  late TextEditingController _searchTextFieldController;
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
     // print(_deviceHeight);
     // print(_deviceWidth);
+    _searchTextFieldController = TextEditingController();
     return _buildUI();
   }
 
@@ -22,7 +25,10 @@ class MainScreen extends ConsumerWidget {
         height: _deviceHeight,
         width: _deviceWidth,
         child: Stack(
-          children: [_backgroundWidget()],
+          children: [
+            _backgroundWidget(),
+            _foregroundWidget(),
+          ],
         ),
       ),
     );
@@ -44,6 +50,59 @@ class MainScreen extends ConsumerWidget {
         child: Container(
           decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
         ),
+      ),
+    );
+  }
+
+  Widget _foregroundWidget() {
+    return Container(
+      padding: EdgeInsets.fromLTRB(0, _deviceHeight! * 0.02, 0, 0.0),
+      width: _deviceHeight! * 0.88,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          _topBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return Container(
+        height: _deviceHeight! * 0.88,
+        decoration: BoxDecoration(
+            color: Colors.black54, borderRadius: BorderRadius.circular(20.0)),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _searchFieldWidget(),
+          ],
+        ));
+  }
+
+  Widget _searchFieldWidget() {
+    final _border = InputBorder.none;
+    return Container(
+      width: _deviceWidth! * 0.50,
+      height: _deviceHeight! * 0.05,
+      child: TextField(
+        controller: _searchTextFieldController,
+        onSubmitted: (_input) {},
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        decoration: InputDecoration(
+            focusedBorder: _border,
+            border: _border,
+            prefixIcon: Icon(Icons.search, color: Colors.white24),
+            hintStyle: TextStyle(color: Colors.white54),
+            filled: false,
+            fillColor: Colors.white24,
+            hintText: 'Search...'),
       ),
     );
   }
