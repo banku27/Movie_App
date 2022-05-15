13f7c1ca4ae52d9051f2340c15a176d31fe557cc
banku27 <pankajparihar123321@gmail.com>
2022-05-15 10:59:10 +0530

    background ui

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -8,14 +10,41 @@ class MainScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
-    print(_deviceHeight);
-    print(_deviceWidth);
+    // print(_deviceHeight);
+    // print(_deviceWidth);
     return _buildUI();
   }
 
   Widget _buildUI() {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        child: Stack(
+          children: [_backgroundWidget()],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: DecorationImage(
+            image: NetworkImage(
+                'https://images3.alphacoders.com/882/thumb-1920-882548.jpg'),
+            fit: BoxFit.cover),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.black.withOpacity(0.2)),
+        ),
+      ),
     );
   }
 }
