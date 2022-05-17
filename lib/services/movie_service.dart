53f216c7d76445d6896c8197c5b150288881085c
banku27 <pankajparihar123321@gmail.com>
2022-05-17 13:14:21 +0530

    controller and rest api

--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:movie_app/models/movie.dart';
 import 'package:movie_app/services/http__service.dart';
 
 class MovieService {
@@ -8,5 +10,19 @@ class MovieService {
 
   MovieService() {
     _http = getIt.get<HttpService>();
+  }
+
+  Future<List<Movie>> getPopularMovies({required int page}) async {
+    Response _response =
+        await _http!.get('/movie/popular', query: {'page': page});
+    if (_response.statusCode == 200) {
+      Map _data = _response.data;
+      List<Movie> _movies = _data['results'].map<Movie>((_movieData) {
+        return Movie.fromJson(_movieData);
+      }).toList();
+      return _movies;
+    } else {
+      throw Exception("Couldn't load popular movies.");
+    }
   }
 }
